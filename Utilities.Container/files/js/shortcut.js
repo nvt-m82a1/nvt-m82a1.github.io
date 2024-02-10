de0031a246c4acfe5e5b44552231b4dc6ce22d29
NV Tuyen <nvt.m82a1@gmail.com>
2024-02-10 19:07:20 +0700

    Add fonts and size

--- Utilities.Container/files/js/shortcut.js
+++ Utilities.Container/files/js/shortcut.js
@@ -1,58 +1,70 @@
 (function () {
     var map = {
         "KeyH": {
+            name: "Focus header",
             run: hotkey_ctrlh,
             "KeyH": {
+                name: "Open homepage",
                 run: hotkey_ctrlh_h
             },
             "KeyG": {
+                name: "Open git",
                 run: hotkey_ctrlh_g
             },
             "KeyV": {
+                name: "Open version",
                 run: hotkey_ctrlh_v
             }
         },
         "KeyM": {
+            name: "Focus menu",
             run: hotkey_ctrlm,
             "ArrowUp": {
+                name: "Move up",
                 run: hotkey_ctrlm_arrowup,
                 skip: true,
             },
             "ArrowDown": {
+                name: "Move down",
                 run: hotkey_ctrm_arrowdown,
                 skip: true,
             },
             "Enter": {
+                name: "Select menu",
                 run: hotkey_ctrm_enter,
                 skip: true,
             }
         },
         "KeyP": {
+            name: "Focus page",
             run: hotkey_ctrlp,
             "ArrowUp": {
+                name: "Move up",
                 run: hotkey_ctrlp_arrowup,
                 skip: true,
             },
             "ArrowDown": {
+                name: "Move down",
                 run: hotkey_ctrp_arrowdown,
                 skip: true,
             },
             "Enter": {
+                name: "Select item",
                 run: hotkey_ctrp_enter,
                 skip: true,
             }
         },
         "KeyF": {
+            name: "Focus footer",
             run: hotkey_ctrlf,
             "KeyM": {
+                name: "Open mail",
                 run: hotkey_ctrlf_m
             }
         },
         "KeyK": {
-            run: hotkey_ctrlm,
-            "KeyM": {
-                run: hotkey_ctrlf_m
-            }
+            name: "Open textbox",
+            run: hotkey_ctrlk
         }
     }
 
@@ -68,31 +80,15 @@
 
     // app hotkey
     var apphotkey = document.getElementById("app_hotkey");
-    var apphotkey_nextReset;
 
-    function setAppHotkey(text, nextReset = true) {
+    function setAppHotkey(text) {
         apphotkey.textContent = text;
-        apphotkey_nextReset = nextReset;
-    }
-
-    function addAppHotkey(hotkey) {
-        var text = apphotkey.textContent;
-        apphotkey.textContent = (apphotkey_nextReset || text.length === 0) ? hotkey : text + " + " + hotkey;
-        apphotkey_nextReset = false;
     }
     // end app hotkey
 
     function doc_OnKeydown(e) {
         if (e.ctrlKey && targetKey[e.code] !== undefined) {
-            if (targetKey[e.code].skip === true) {
-                targetKey[e.code].run();
-            }
-            else {
-                addAppHotkey("Ctrl+" + e.code);
-                targetKey = targetKey[e.code];
-                targetKey.run();
-            }
-            e.preventDefault();
+            defaultOnKeydown(e);
         }
 
         if (!e.ctrlKey && !e.shiftKey) {
@@ -105,41 +101,42 @@
 
                 default:
                     if (targetKey !== undefined && targetKey[e.code] !== undefined) {
-                        if (targetKey[e.code].skip === true) {
-                            targetKey[e.code].run();
-                        }
-                        else {
-                            addAppHotkey(e.code);
-                            targetKey = targetKey[e.code];
-                            targetKey.run();
-                        }
-                        e.preventDefault();
+                        defaultOnKeydown(e);
                     }
                     break;
             }
         }
+
+        function defaultOnKeydown(e) {
+            if (targetKey[e.code].skip === true) {
+                targetKey[e.code].run();
+            }
+            else {
+                targetKey = targetKey[e.code];
+                setAppHotkey(targetKey.name);
+                targetKey.run();
+            }
+            e.preventDefault();
+        }
+
     }
 
     // ctrl h
     function hotkey_ctrlh() {
-        setAppHotkey("Focus header");
         document.getElementById("header").focus();
     }
 
     function hotkey_ctrlh_h() {
-        setAppHotkey("Open homepage");
         document.getElementById("app_name").click();
         resetHotkey();
     }
 
     function hotkey_ctrlh_g() {
-        setAppHotkey("Open git");
         document.getElementById("app_git").click();
         resetHotkey();
     }
 
     function hotkey_ctrlh_v() {
-        setAppHotkey("Open version");
         document.getElementById("app_version").click();
         resetHotkey();
     }
@@ -177,7 +174,6 @@
     }
 
     function hotkey_ctrm_enter() {
-        setAppHotkey("");
         menuItemFocus.click();
         menuItemFocus.classList.remove("menu-select")
         resetHotkey();
@@ -208,9 +204,6 @@
 
     // ctrl k
     function hotkey_ctrlk() {
-    }
-
-    function hotkey_ctrlk_m() {
     }
     // end ctrl k
 
